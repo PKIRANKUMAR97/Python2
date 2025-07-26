fb4908d1e6f31328a9565256475919c35132b8c4
PKIRANKUMAR97 <poluparthi.kirankumar@gmail.com>
2025-07-26 15:04:46 +0530

    completed day 2

--- Day2/conditionalstatements.py
+++ Day2/conditionalstatements.py
@@ -2,8 +2,63 @@
 # voter eligibility
 # age > 18
 
-age = 15
-if age >=18:
-    print("eligible for vote")
-else :
-    print("not eligible for vote")
+# age = 34
+# if age >=18:
+#     print("eligible for vote")
+# else :
+#     print("not eligible for vote")
+
+# example 2
+# if True :
+#     print("true")
+# else:
+#     print("false")
+
+# example 3
+
+# if 1:
+#     print("one ")
+# else:
+#     print("not one  ")
+#
+
+# example 4 : number even or odd
+
+# x = 101
+# if x %2==0:
+#     print("even")
+# else:
+#     print("odd")
+
+#example 5 : if else in single lime -- ternary operator
+
+# c= 101311232532453642574756876799870
+# print("even") if c%2==0 else print("odd")
+
+# example 6 : multiple statements in the single line --ternary operator
+# c =201
+# {print("HANA"),print("CLOUD")} if c%2!=0 else {print("HANA"),print("ONPREM")}
+
+# example 7 -- multiple conditions using elif
+weekno=9
+
+if weekno==1:
+    print("Sunday")
+elif weekno==2:
+    print("Monday")
+elif weekno==3:
+    print("Tuesday")
+elif weekno==4:
+    print("Wednesday")
+elif weekno==5:
+    print("Thursday")
+elif weekno==6:
+    print("Friday")
+elif weekno==7:
+    print("Saturday")
+else:
+    print("Please enter a valid weekno")
+
+
+
+
