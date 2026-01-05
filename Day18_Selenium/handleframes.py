ebd160b493175dcae95f1d10f0bd8ab22eadab97
kiran <poluparthi.kirankumar@gmail.com>
2026-01-05 21:37:15 +0530

    handling frames in python selenium

---
+++ Day18_Selenium/handleframes.py
@@ -0,0 +1,22 @@
+import time
+
+from selenium import webdriver
+from selenium.common import TimeoutException
+from selenium.webdriver import Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+ser_obj = Service(r"C:\browserdrivers\chromedriver-win64\chromedriver-win64\chromedriver.exe")
+driver=webdriver.Chrome(service=ser_obj)  ## we are launching the browser
+mywait = WebDriverWait(driver,10,ignored_exceptions=[Exception])
+
+driver.get("https://the-internet.herokuapp.com/iframe")
+driver.maximize_window()
+
+driver.switch_to.frame("mce_0_ifr")
+
+time.sleep(5)
+
